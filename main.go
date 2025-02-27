f3162387293fd8d1fd81a98b7ee8da318dfdbe3e
CallMeChero <david.ceranic@gmail.com>
2025-02-27 13:33:51 +0100

    Implement embedded interfaces

--- main.go
+++ main.go
@@ -14,26 +14,47 @@ type saver interface {
 	Save() error
 }
 
+// type displayer interface {
+// 	Display()
+// }
+
+type outputtable interface {
+	saver
+	Display()
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display
+// }
+
 func main() {
 	title, content := getUserNoteData()
 	todoText := getUserInput("Todo text: ")
 	todo, err := todo.New(todoText)
+
+	if err != nil {
+		return
+	}
+
 	userNote, err := note.New(title, content)
 
-	todo.Display()
-	err = saveData(todo)
+	if err != nil {
+		return
+	}
+
+	err = outputData(todo)
 
 	if err != nil {
 		return
 	}
 
-	userNote.Display()
-	err = saveData(userNote)
+	outputData(userNote)
+}
 
-	if err != nil {
-		return
-	}
-
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
 }
 
 func saveData(data saver) error {
